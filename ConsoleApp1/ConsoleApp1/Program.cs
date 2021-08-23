74d463cea904c04f64e3fa0eb1b959565dabe573
soumik-07 <soumikkahali@gmail.com>
2021-08-23 16:05:22 +0530

    Factorial code

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, fact = 1, i;
+            Console.WriteLine("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(i=1;i<=num;i++)
+            {
+                fact = fact * i;
+
+            }
+            Console.WriteLine("Factorial of " + num + " is: " + fact);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Fibo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, num;
+            Console.Write("Enter the number of elements: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(n1 + "," + n2 + ",");    
+            
+            for (i = 2; i < num; ++i) //loop starts from 2 because 0 and 1 are already printed    
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + ",");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.ReadLine();
+        }
+    }
+}
